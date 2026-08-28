e068247a1f041f275017ca254b4bb6437a049a6a
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-08-28 12:52:25 +0800

    alvistack/v18.2.8
        git clean -xdf
        git submodule sync --recursive
        git submodule update --recursive --init
        git submodule foreach --recursive git checkout -- .
        git submodule foreach --recursive git clean -xdf
        ./make-debs.sh
        cp /tmp/release/Ubuntu/WORKDIR/*ceph*18.2.8*.{bz2,xz,dsc} /osc/home\:alvistack/ceph-ceph-18.2.8/
        cp ceph.spec /osc/home\:alvistack/ceph-ceph-18.2.8/ceph_18.2.8-1.spec
        cp ceph.rpmlintrc /osc/home\:alvistack/ceph-ceph-18.2.8/
        cp debian/patches/*.patch /osc/home\:alvistack/ceph-ceph-18.2.8/
        cp _constraints /osc/home\:alvistack/ceph-ceph-18.2.8/
        rm -rf /tmp/release/Ubuntu/WORKDIR
    
    See https://github.com/spdk/spdk/issues/2637
    See https://tracker.ceph.com/issues/56610
    See https://build.opensuse.org/package/show/filesystems:ceph:quincy:upstream/ceph
    See https://src.fedoraproject.org/rpms/ceph/tree/rawhide
    See https://packages.ubuntu.com/source/lunar/ceph
    See https://bugzilla.stage.redhat.com/show_bug.cgi?id=2259639
    See https://github.com/ceph/ceph/pull/58199
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- make-debs.sh
+++ make-debs.sh
@@ -19,7 +19,7 @@ set -xe
 . /etc/os-release
 base=${1:-/tmp/release}
 releasedir=$base/$NAME/WORKDIR
-rm -fr $(dirname $releasedir)
+#rm -fr $(dirname $releasedir)
 
 # git describe provides a version that is
 # a) human readable
@@ -32,8 +32,8 @@ rm -fr $(dirname $releasedir)
 # an argument to this script.
 #
 if [ -z "${2}" ]; then
-    vers=$(git describe --match "v*" | sed s/^v//)
-    dvers=${vers}-1
+    vers="18.2.8"
+    dvers="100:${vers}-1"
 else
     vers=${2}
     dvers=${vers}-1${VERSION_CODENAME}
