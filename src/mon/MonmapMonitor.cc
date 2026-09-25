ceph/ceph-ci

mon: do not propose on NOp mon settings
Change-Id: dedb8af6-9837-4384-9bfc-f087f92847e2
Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

src/mon/MonmapMonitor.cc
@@ -1268,6 +1268,11 @@ bool MonmapMonitor::prepare_command(MonOpRequestRef op)
         err = -EINVAL;
         goto reply_no_propose;
       }
+      if (c == pending_map.auth_service_cipher) {
+        err = 0;
+        ss << "already set";
+        goto reply_no_propose;
+      }
       pending_map.auth_service_cipher = c;
     } else if (name == "auth_allowed_ciphers") {
       std::vector<std::string> v;
@@ -1282,11 +1287,21 @@ bool MonmapMonitor::prepare_command(MonOpRequestRef op)
         ciphers.push_back(c);
       }
       std::sort(ciphers.begin(), ciphers.end());
+      if (ciphers == pending_map.auth_allowed_cipher) {
+        err = 0;
+        ss << "already set";
+        goto reply_no_propose;
+      }
       pending_map.auth_allowed_ciphers = std::move(ciphers);
     } else if (name == "auth_preferred_cipher") {
       int c = CryptoManager::get_key_type(value);
       if (c < 0) {
         err = -EINVAL;
+        goto reply_no_propose;
+      }
+      if (c == pending_map.auth_preferred_cipher) {
+        err = 0;
+        ss << "already set";
         goto reply_no_propose;
       }
       pending_map.auth_preferred_cipher = c;

alvistack/ceph-ceph

(cherry picked from commit 80eda53af4462de8e9e87f6c50d857cad8fb0838)

src/mon/MonmapMonitor.cc
@@ -1262,6 +1262,11 @@ bool MonmapMonitor::prepare_command(MonOpRequestRef op)
         err = -EINVAL;
         goto reply_no_propose;
       }
+      if (c == pending_map.auth_service_cipher) {
+        err = 0;
+        ss << "already set";
+        goto reply_no_propose;
+      }
       pending_map.auth_service_cipher = c;
     } else if (name == "auth_allowed_ciphers") {
       std::vector<std::string> v;
@@ -1276,11 +1281,21 @@ bool MonmapMonitor::prepare_command(MonOpRequestRef op)
         ciphers.push_back(c);
       }
       std::sort(ciphers.begin(), ciphers.end());
+      if (ciphers == pending_map.auth_allowed_ciphers) {
+        err = 0;
+        ss << "already set";
+        goto reply_no_propose;
+      }
       pending_map.auth_allowed_ciphers = std::move(ciphers);
     } else if (name == "auth_preferred_cipher") {
       int c = CryptoManager::get_key_type(value);
       if (c < 0) {
         err = -EINVAL;
+        goto reply_no_propose;
+      }
+      if (c == pending_map.auth_preferred_cipher) {
+        err = 0;
+        ss << "already set";
         goto reply_no_propose;
       }
       pending_map.auth_preferred_cipher = c;